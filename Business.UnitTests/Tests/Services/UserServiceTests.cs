80b5ae98f2e13a073496d9ef44cbab6151e5e3ac
mahmut kasimoglu <mkasimoglu@flyingdonkey.com.au>
2022-01-20 22:29:49 +0300

    Implemented Failing scenario for registeration

--- Business.UnitTests/Tests/Services/UserServiceTests.cs
+++ Business.UnitTests/Tests/Services/UserServiceTests.cs
@@ -21,44 +21,68 @@ namespace Business.UnitTests.Tests.Services
     public class UserServiceTests
     {
         private IUserService _userService;
-        private Mock<IUnitOfWork> _mockUow = new Mock<IUnitOfWork>();
-        private Mock<ITokenHelper> _tokenHelper = new Mock<ITokenHelper>();
-        private Mock<IRepository<User>> _mockUserRepository = new Mock<IRepository<User>>();
-        private Mock<IRepository<Role>> _mockRoleRepository = new Mock<IRepository<Role>>();
+        private Mock<IUnitOfWork> _mockUow;
+        private Mock<ITokenHelper> _tokenHelper;
+        private Mock<IRepository<User>> _mockUserRepository;
+        private Mock<IRepository<Role>> _mockRoleRepository;
         private List<Role> _roleList = new List<Role> { new Role { Id = Guid.NewGuid() } };
+
+        UserRegisterDto newUser = new UserRegisterDto
+        {
+            Email = "john_doe@gmail.com",
+            FirstName = "John",
+            LastName = "Doe",
+            UserName = "john317",
+            Password = "doe123"
+        };
 
         [SetUp]
         public void SetUp()
         {
+            _mockUow = new Mock<IUnitOfWork>();
+            _tokenHelper = new Mock<ITokenHelper>();
+            _mockUserRepository = new Mock<IRepository<User>>();
+            _mockRoleRepository = new Mock<IRepository<Role>>();
             _mockUow.Setup(x => x.GetEntityRepository<User>()).Returns(_mockUserRepository.Object);
             _mockUow.Setup(x => x.GetEntityRepository<Role>()).Returns(_mockRoleRepository.Object);
             _userService = new UserService(_mockUow.Object, _tokenHelper.Object);
+
+            _mockRoleRepository.Setup(x => x.GetMany(It.IsAny<Expression<Func<Role, bool>>>())).Returns(_roleList);
+            _tokenHelper.Setup(x => x.CreateToken(It.IsAny<User>(), It.IsAny<List<Role>>())).Returns(new AccessToken { Token = "JwtToken" });
         }
 
         [Test]
         public void Register_ShouldBeSuccessful_WhenUsernameAndEmailIsUnique()
         {
-            //arrange
-            var newUser = new UserRegisterDto
-            {
-                Email = "john_doe@gmail.com",
-                FirstName = "John",
-                LastName = "Doe",
-                UserName = "john317",
-                Password = "doe123"
-            };
 
             _mockUserRepository.Setup(x => x.Get(It.IsAny<Expression<Func<User, bool>>>())).Returns<User>(null);
-            _mockRoleRepository.Setup(x => x.GetMany(It.IsAny<Expression<Func<Role, bool>>>())).Returns(_roleList);
-            _tokenHelper.Setup(x => x.CreateToken(It.IsAny<User>(), It.IsAny<List<Role>>())).Returns(new AccessToken { Token = "JwtToken"});
 
-            //act
+
             var result = _userService.Register(newUser);
 
             //assert
             _mockUserRepository.Verify(x => x.Create(It.IsAny<User>()), Times.Once);
             Assert.True(result.Result.Success);
-            
+        }
+
+        [Test]
+        public void Register_ShouldBeUnSuccessful_WhenUsernameOrEmailExists()
+        {
+            _mockUserRepository.Setup(x => x.Get(It.IsAny<Expression<Func<User, bool>>>())).Returns(new Mock<User>().Object);
+
+
+            var result = _userService.Register(newUser);
+
+
+            _mockUserRepository.Verify(x => x.Create(It.IsAny<User>()), Times.Never);
+            Assert.False(result.Result.Success);
+        }
+
+
+        [Test]
+        public void Login_ShouldBeSuccessful_WhenCredentialsAreValid()
+        {
+
         }
     }
 }
